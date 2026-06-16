ab55734d9c55e53c5388c5d42fb64138f7677fc7
Anvita Kallam <kranvita007@gmail.com>
2026-06-16 08:55:02 -0500

    Use clearbee and make bee flight more dramatic (right on sponsors).

--- src/components/Parallax/ScrollBeeCharacter.tsx
+++ src/components/Parallax/ScrollBeeCharacter.tsx
@@ -22,13 +22,13 @@ const BeeSegment = ({
     startScroll={startScroll}
     endScroll={endScroll}
     translateY={[0, speed * -11]}
-    translateX={[0, speedX * -10]}
-    rotate={facingRight ? [-10, 6] : [6, -10]}
+    translateX={[0, speedX * -32]}
+    rotate={facingRight ? [-24, 14] : [14, -24]}
     className={`cs-bee-segment ${className}`}
   >
     <div className="cs-bee-wrap">
       <img
-        src="/images/bee-character.png"
+        src="/images/clearbee.png"
         alt=""
         className={`cs-bee-character ${facingRight ? "cs-bee-facing-right" : "cs-bee-facing-left"}`}
         draggable={false}
@@ -46,30 +46,40 @@ const ScrollBeeCharacter = ({ scrollEnd, heroHeight }: ScrollBeeCharacterProps) 
   const scrollSpan = Math.max(scrollEnd - heroHeight, 1);
   const seg1End = heroHeight + scrollSpan * 0.33;
   const seg2End = heroHeight + scrollSpan * 0.66;
+  const sponsorsStart = heroHeight + scrollSpan * 0.62;
+  const sponsorsEnd = heroHeight + scrollSpan * 0.74;
 
   return (
     <>
       <BeeSegment
         startScroll={heroHeight}
         endScroll={seg1End}
-        speed={-6}
-        speedX={-14}
+        speed={-8}
+        speedX={-32}
         className="cs-bee-a"
         facingRight
       />
       <BeeSegment
         startScroll={seg1End}
         endScroll={seg2End}
-        speed={-8}
-        speedX={12}
+        speed={-10}
+        speedX={36}
         className="cs-bee-b"
+        facingRight={false}
+      />
+      <BeeSegment
+        startScroll={sponsorsStart}
+        endScroll={sponsorsEnd}
+        speed={-11}
+        speedX={52}
+        className="cs-bee-sponsors"
         facingRight={false}
       />
       <BeeSegment
         startScroll={seg2End}
         endScroll={scrollEnd}
-        speed={-7}
-        speedX={-16}
+        speed={-9}
+        speedX={-44}
         className="cs-bee-c"
         facingRight
       />
